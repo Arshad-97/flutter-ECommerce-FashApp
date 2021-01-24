edb697e657216f8f19af8dbe70fd272462e9b17a
Arshad-97 <agmarshad97@gmail.com>
2021-01-24 15:23:41 +0530

    Created Carousel Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() {
   runApp(
@@ -17,8 +18,28 @@ class HomePage extends StatefulWidget{
 class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context){
+    Widget image_carousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+          AssetImage('images/m2.jpg'),
+        ],
+        autoplay: false,
+        // animationCurve: Curves.fastOutSlowIn,
+        // animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        dotColor: Colors.red,
+        indicatorBgPadding: 2.0,
+      )
+    );
     return Scaffold(
       appBar: new AppBar(
+        elevation: 0.1,
         backgroundColor: Colors.red,
         title: Text('FashApp'),
         actions: <Widget>[
@@ -36,9 +57,71 @@ class _HomePageState extends State<HomePage>{
                  backgroundColor: Colors.grey,
                  child: Icon(Icons.person, color: Colors.white,),
                ),
-             ),),
+             ),
+             decoration: new BoxDecoration(
+               color: Colors.red
+             ),
+             ),
+//            Body
+              InkWell(
+                onTap: (){},
+                child: ListTile(
+                  title: Text('Home Page'),
+                  leading: Icon(Icons.home),
+                )
+              ),
+              InkWell(
+                onTap: (){},
+                child: ListTile(
+                  title: Text('My Account'),
+                  leading: Icon(Icons.person),
+                )
+              ),
+              InkWell(
+                onTap: (){},
+                child: ListTile(
+                  title: Text('My Order'),
+                  leading: Icon(Icons.shopping_basket),
+                )
+              ),
+              InkWell(
+                onTap: (){},
+                child: ListTile(
+                  title: Text('catergoris'),
+                  leading: Icon(Icons.dashboard),
+                )
+              ),
+              InkWell(
+                onTap: (){},
+                child: ListTile(
+                  title: Text('favourite'),
+                  leading: Icon(Icons.favorite),
+                )
+              ),
+
+              Divider(),
+              InkWell(
+                onTap: (){},
+                child: ListTile(
+                  title: Text('Setting'),
+                  leading: Icon(Icons.settings, color: Colors.blue,),
+                )
+              ),
+              InkWell(
+                onTap: (){},
+                child: ListTile(
+                  title: Text('About'),
+                  leading: Icon(Icons.help, color: Colors.green,),
+                )
+              )
           ],
         )
+      ),
+
+      body: new ListView(
+        children: <Widget>[
+          image_carousel
+        ]
       ),
     );    
   }
